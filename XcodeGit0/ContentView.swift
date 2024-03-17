8a2200946f23b6eb6455703eefde29b7048fd7a8
blitz2002jp <blitz2002jp@gmail.com>
2024-03-17 12:24:36 +0900

    Initial Commit

---
+++ XcodeGit0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeGit0
+//
+//  Created by masazumi oeda on 2024/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeGit0/XcodeGit0App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGit0App.swift
+//  XcodeGit0
+//
+//  Created by masazumi oeda on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGit0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
